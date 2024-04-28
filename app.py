0d4039c6688de1a5e06b6dd921b7bbb1cead24cd
aenosys <aenosys@gmail.com>
2024-04-28 10:56:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# Define the product data directly in the Flask app
+products = {
+    "products": [
+        {
+            "id": 1,
+            "name": "Product 1",
+            "price": 20.99,
+            "description": "This is the description of Product 1."
+        },
+        {
+            "id": 2,
+            "name": "Product 2",
+            "price": 15.99,
+            "description": "This is the description of Product 2."
+        }
+    ]
+}
+
+# Define a route to serve the products JSON data
 @app.route('/')
-def hello_world():
-    return 'Hello, Dockerized Python Flask App!'
+def get_products():
+    return jsonify(products)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
